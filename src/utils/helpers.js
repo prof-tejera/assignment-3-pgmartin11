9ad8c636def6d6dbe0fe66c4eea60b7aec953565
Peter Martin <pgmartin@Peters-MacBook-Pro.local>
2022-11-27 11:12:58 -0500

    added new helper function for id creation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -92,3 +92,17 @@ export const formattedDateTime = () => {
    return (d.getMonth()+1) + "/" + d.getDate() + "/" + d.getFullYear() +
              " " + d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
 }
+
+
+/*
+ * from Lecture 8 notes
+ */
+export const makeId = () => {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < 20; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
