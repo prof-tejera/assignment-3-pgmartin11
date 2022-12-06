16f4e718b549a1fa19b815d8239c55562928d3a3
Peter Martin <pgmartin@Peters-MacBook-Pro.local>
2022-12-05 22:22:07 -0500

    some additiona style tweaks

--- src/components/timers/TimerStyles.js
+++ src/components/timers/TimerStyles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 export const TimerDescription = styled.textarea`
   border: none;
-  font-size: 16px;
+  font-size: 14px;
   resize: vertical;
   width: 100%;
 `;
