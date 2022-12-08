072871977dc655e250670e996ac696349c6d7f66
Peter Martin <pgmartin@Peters-MacBook-Pro.local>
2022-12-07 20:30:59 -0500

    more style tweaks

--- src/components/timers/TimerStyles.js
+++ src/components/timers/TimerStyles.js
@@ -2,8 +2,10 @@ import styled from "styled-components";
 
 
 export const TimerDescription = styled.textarea`
-  border: none;
   font-size: 14px;
   resize: vertical;
+  margin-top: 10px;
+  border: 1px solid gray;
+  border-radius: 5px;
   width: 100%;
 `;
