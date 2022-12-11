fbacca93321e3dd5045d86e7d295d217c7020408
Peter Martin <pgmartin@Peters-MacBook-Pro.local>
2022-12-11 14:08:20 -0500

    ug fixes for persisted state hook

--- src/hooks.js
+++ src/hooks.js
@@ -4,13 +4,12 @@ export const usePersistedStatePolling = (storageKey, fallbackValue) => {
   const [value, setValue] = useState(() => {
     const storedValue = window.localStorage.getItem(storageKey);
 
-    if (storedValue === null || !storedValue) {
-      console.log('returning fallback', fallbackValue);
+    if (storedValue === null || typeof storedValue === 'undefined') {
+      console.log('fallback', fallbackValue);
       return fallbackValue;
     }
 
     try {
-      console.log('storedValue', storedValue);
       return JSON.parse(storedValue);
     } catch (e) {
       console.log('Error parsing stored value', e);
@@ -19,18 +18,23 @@ export const usePersistedStatePolling = (storageKey, fallbackValue) => {
   });
 
   useEffect(() => {
-    //const i = setInterval(() => {
-    const i = setTimeout(() => {
-      if (value) {
-//XXX
-//console.log('localStorage key: ',storageKey+'value: '+value);
+ //   const i = setInterval(() => {
+ //   const t = setTimeout(() => {
+      if (value !== null || typeof value !== 'undefined') {
         window.localStorage.setItem(storageKey, JSON.stringify(value));
       } else {
         window.localStorage.removeItem(storageKey);
       }
-    }, 2000);
+  //  }, 2000);
 
-    //return () => clearInterval(i);
+    // return () => clearInterval(i);
+    /*
+    return () => {
+      if (t) {
+        clearTimeout(t);
+      }
+    };
+    */
   }, [value]);
 
   return [
