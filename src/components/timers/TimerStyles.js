1ddefa5ae6f150f5631325f8465f26464fb2de63
Peter Martin <pgmartin@Peters-MacBook-Pro.local>
2022-12-12 21:28:40 -0500

    style tweaks for timer description textarea

--- src/components/timers/TimerStyles.js
+++ src/components/timers/TimerStyles.js
@@ -2,8 +2,10 @@ import styled from "styled-components";
 
 
 export const TimerDescription = styled.textarea`
+  font-family: Arial, sans-serif;
   font-size: 14px;
   resize: vertical;
+  padding: 5px;
   margin-top: 10px;
   border: 1px solid gray;
   border-radius: 5px;
